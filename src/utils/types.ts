154fd1504bf579f5cd281c326cc5ac6bfbddc1ee
Patrick Mvula <mvlapatrick@gmail.com>
2025-03-17 14:49:50 +0200

    refactor: moved lib contents to a utils folder

---
+++ src/utils/types.ts
@@ -0,0 +1,35 @@
+export type FileAction = {
+  file: File;
+  fileName: string;
+  fileSize: number;
+  from: string;
+  fileType: string;
+  isError?: boolean;
+  url?: string;
+  output?: any;
+  outputBlob?: Blob;
+};
+
+export enum VideoFormats {
+  MP4 = "mp4",
+  MKV = "mkv",
+  AVI = "avi",
+  MOV = "mov",
+  FLV = "flv",
+  WEBM = "webm",
+}
+
+export enum QualityType {
+  High = "15",
+  Medium = "18",
+  Low = "20",
+}
+
+export type VideoInputSettings = {
+  quality: QualityType;
+  videoType: VideoFormats;
+  customEndTime: number;
+  customStartTime: number;
+  removeAudio: boolean;
+  twitterCompressionCommand: boolean;
+};
