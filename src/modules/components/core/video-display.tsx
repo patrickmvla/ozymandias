b8d62493d38ca0831b7ff46b8c18f9283e645566
Patrick Mvula <mvlapatrick@gmail.com>
2025-03-17 13:05:10 +0200

    feat: video display

---
+++ src/modules/components/core/video-display.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  videoUrl: string;
+}
+
+export const VideoDisplay = ({ videoUrl }: Props) => {
+  <video
+    id="compress-video-player"
+    className="h-full w-full rounded-3xl"
+    controls
+  >
+    <source src={videoUrl} type="video/mp4" />
+    your browser does not support the video tag
+  </video>;
+};
