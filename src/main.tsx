c7a5ca0afecdc3d3bfa09dd7c91734575585d77a
Patrick Mvula <mvlapatrick@gmail.com>
2025-03-18 13:08:21 +0200

    feat(UI): added a new background

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,15 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HomePage />
-    <Toaster />
+    <body
+      className={`bg-gray-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px] h-full w-full`}
+    >
+      <div className="fixed w-full h-full top-0 bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]">
+        <div className="relative">
+          <HomePage />
+          <Toaster />
+        </div>
+      </div>
+    </body>
   </StrictMode>
 );
